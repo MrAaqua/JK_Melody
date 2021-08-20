065cc4e12482df28ac42f8839f49ea0ae0fdffd2
MrAaqua <59654010+MrAaqua@users.noreply.github.com>
2021-08-20 12:39:53 +0500

    Реализация модального окна, с планировкой квартир

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ $(document).ready(function () {
     var floorPath = $(".home-image path");
     var counterUp = $(".counter-up");
     var counterDown = $(".counter-down");
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
+
+/*     var flatsPath = $(".flats path");
+    var currentFlats = 9;
+    var flatLink = $(".flat-link"); */
 
     // функция при наведении мышью на этаж
     floorPath.on("mouseover", function () {
@@ -10,6 +17,17 @@ $(document).ready(function () {
         currentFloor = $(this).attr('data-floor'); // получаем значение текущего этажа
         $(".counter").text(currentFloor); // записываем в счетчик значение этажа
     });
+
+    floorPath.on('click', toggleModal);
+    modalCloseButton.on('click', toggleModal);
+    viewFlatsButton.on('click', toggleModal);
+
+
+/*     flatsPath.on("mouseover", function () {
+        flatsPath.removeClass("current-flats"); // удаляем выделение этажа на схеме
+        currentFlats = $(this).attr('id-flats'); // получаем значение текущего этажа
+        $(".counter").text(currentFlats); // записываем в счетчик значение этажа
+    }); */
 
     counterUp.on("click", function() { // отслеживаем клик вверх
         if (currentFloor < 18) { 
@@ -31,6 +49,12 @@ $(document).ready(function () {
             floorPath.removeClass("current-floor");
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
-    })
+    });
+
+
+
+    function toggleModal(){
+        modal.toggleClass('is-open');
+    };
 });
 
